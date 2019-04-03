1db2da5f0c537d8c3f3e6dabe04f3930e3a9ad68
gaowenjie7 <yidong-ios16>
2019-04-03 16:11:37 +0800

    live hot reload

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@
 
 // gulpfile.js
 var gulp = require('gulp');
+var connect = require('gulp-connect');
+
 
 var paths = {
   src: 'src/**/*',
@@ -18,20 +20,37 @@ var paths = {
   distJS: 'dist/**/*.js'
 };
 
-gulp.task('html', function () {
-  return gulp.src(paths.srcHTML).pipe(gulp.dest(paths.tmp));
+gulp.task('html', function (done) {
+   gulp.src(paths.srcHTML).pipe(gulp.dest(paths.tmp)).pipe(connect.reload());
+   done()
 });
 
-gulp.task('css', function () {
-    return gulp.src(paths.srcCSS).pipe(gulp.dest(paths.tmp));
+gulp.task('css', function (done) {
+    gulp.src(paths.srcCSS).pipe(gulp.dest(paths.tmp)).pipe(connect.reload());
+    done()
 });
 
-gulp.task('js', function () {
-    return gulp.src(paths.srcJS).pipe(gulp.dest(paths.tmp));
+gulp.task('js', function (done) {
+    gulp.src(paths.srcJS).pipe(gulp.dest(paths.tmp)).pipe(connect.reload());
+    done()
 });
 
-gulp.task('copy',gulp.series(['html','css','js'],function(){
-    console.log('copy complete')
-}));
+gulp.task('watch',function(done){
+  gulp.watch(paths.srcHTML,gulp.series('html'))
+  gulp.watch(paths.srcCSS,gulp.series('css'))
+  gulp.watch(paths.srcJS,gulp.series('js'))
+  console.log('files changing ...')
+  done()
+})
+
+gulp.task('connect',function(done){
+  connect.server({
+    livereload:true
+  });
+  done()
+})
+
+
+gulp.task('default',gulp.series(['html','css','js','watch','connect']));
 
 
